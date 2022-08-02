a9dbfb69b36164d03bbe3d08684b2b59e6ec2b37
g3th3x <g3th3x@gmail.com>
2022-08-03 02:54:59 +0300

    complete compareArrays function

--- app.js
+++ app.js
@@ -1,33 +1,55 @@
 //Сравнение массивов
+
+function rad1() {
+  document.getElementById("tools1").style.display = "none"; // hide
+  //document.getElementById("tools1").style.display = ""; // show
+}
+
+function rad2() {
+  //document.getElementById("tools1").style.display = "none"; // hide
+  document.getElementById("tools1").style.display = ""; // show
+}
+
+function rad3() {
+  document.getElementById("tools2").style.display = "none"; // hide
+  //document.getElementById("tools1").style.display = ""; // show
+}
+
+function rad4() {
+  //document.getElementById("tools1").style.display = "none"; // hide
+  document.getElementById("tools2").style.display = ""; // show
+}
+
 function compareArrays() {
   let res = "";
-  let data1 = document.querySelector("#ta1").value;
-  //console.log(typeof data1);
-  //data1.replace(/\n/gi, "#").split(/#/);
-  console.log(data1.replace(/\n\r|\n/gi, "#").split(/#/));
-  console.log(typeof data1);
-  let data2 = document.querySelector("#ta2").value;
-  console.log(data2.replace(/^\n\r|\n/gi, "#").split(/#/));
+  let arr1 = document
+    .querySelector("#ta1")
+    .value.replace(/^[\n\r]+|[\n\r]+$/g, "")
+    .split(/[\n\r]+/);
 
-  if (data1 == "" || data2 == "") alert("Введите данные для обработки!");
+  let arr2 = document
+    .querySelector("#ta2")
+    .value.replace(/^[\n\r]+|[\n\r]+$/g, "")
+    .split(/[\n\r]+/);
+  //   console.log(arr1);
+  //   console.log(arr2);
+  if (arr1 == "" || arr2 == "") alert("Введите данные для обработки!");
   else {
-    let inFirst = data1.filter((x) => !data2.includes(x));
-    //let inFirst = Object.values(data1).filter((x) => !data2.includes(x));
-    let inSecond = Object.values(data2).filter((x) => !data1.includes(x));
-    let inBoth = Object.values(data1).filter((x) => data2.includes(x));
-    console.log(inFirst);
-    console.log(inSecond);
-    console.log(inBoth);
-    for (let i = 0; i < inFirst["length"]; i++) {
-      res += inFirst[i] + "\t" + "только в первом списке" + "\n";
+    let inFirst = arr1.filter((x) => !arr2.includes(x));
+    let inSecond = arr2.filter((x) => !arr1.includes(x));
+    let inBoth = arr1.filter((x) => arr2.includes(x));
+    // console.log(inFirst);
+    // console.log(inSecond);
+    // console.log(inBoth);
+    for (let index of inFirst) {
+      res += `${index}\tтолько в первом списке\n`;
     }
-    for (let i = 0; i < inSecond["length"]; i++) {
-      res += inSecond[i] + "\t" + "только во втором списке" + "\n";
+    for (let index of inSecond) {
+      res += `${index}\tтолько во втором списке\n`;
     }
-    for (let i = 0; i < inBoth["length"]; i++) {
-      res += inBoth[i] + "\t" + "в обоих списках" + "\n";
+    for (let index of inBoth) {
+      res += `${index}\tв обоих списках\n`;
     }
-
     return (result.value = res);
   }
 }
